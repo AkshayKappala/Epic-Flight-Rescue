e257d2e3a1b4d5cace9588d0fa79ca6fdbeac531
AkshayKappala <akshaykappala98@gmail.com>
2017-07-27 19:09:05 +0530

    fixed restart bug fixed cloudmovement

--- NewPianoTiles/Assets/Scripts/CloudMovement.cs
+++ NewPianoTiles/Assets/Scripts/CloudMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CloudMovement : MonoBehaviour {
 
+    private void Start()
+    {
+        this.gameObject.transform.SetParent(GameObject.Find("BackgroundImage").transform);
+    }
+
     void FixedUpdate () {
         this.transform.Translate(Vector2.up * Time.deltaTime * 5);
 	}
