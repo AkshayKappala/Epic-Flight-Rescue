04db36db411b3ee5495b31b8bd380c2ad5999521
AkshayKappala <akshaykappala98@gmail.com>
2017-07-27 18:45:46 +0530

    fixed alpha decrease problem

--- NewPianoTiles/Assets/CloudDestroyer.cs
+++ NewPianoTiles/Assets/CloudDestroyer.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudGenerator : MonoBehaviour {
+public class CloudDestroyer : MonoBehaviour {
 
     public void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Clouds"))
         {
-            Instantiate(Resources.Load("Clouds"), GameObject.Find("BackgroundImage").transform.position + new Vector3(0, -22, 0), Quaternion.identity);
+            Destroy(other.gameObject);
         }
     }
-    
 }
